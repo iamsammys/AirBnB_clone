d4ecd4b19608a6784b113e4667c5dddb4e4fdbcb
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-05-13 09:14:07 +0100

    updated console

--- console.py
+++ console.py
@@ -35,13 +35,9 @@ class HBNBCommand(cmd.Cmd):
         """Create command to create a new instance of a class
         """
         line = lines.split()
-        if len(line) < 1:
-            print("** class name missing **")
+        if not HBNBCommand.check_class(line):
             return
         cls = line[0]
-        if cls not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
         new_instance = eval(cls + "()")
         new_instance.save()
         print(new_instance.id)
@@ -50,22 +46,49 @@ class HBNBCommand(cmd.Cmd):
         """Show command to print string representation of instance
         """
         arg = args.split()
-        if len(arg) < 1:
-            print("** class name missing **")
+        if not HBNBCommand.check_class(arg):
             return
-        cls = arg[0]
+        if not HBNBCommand.check_id(arg):
+            return
+        key = "{}.{}".format(arg[0], arg[1])
+        print(HBNBCommand.objects[key])
+
+    def do_destroy(self, args):
+        """deletes an instance based on class name and id
+        """
+        arg = args.split()
+        if not HBNBCommand.check_class(arg):
+            return
+        if not HBNBCommand.check_id(arg):
+            return
+        key = "{}.{}".format(arg[0], arg[1])
+        del HBNBCommand.objects[key]
+
+    def check_class(args):
+        """check if a class was passed and exists
+        """
+        if len(args) < 1:
+            print("** class name missing **")
+            return False
+        cls = args[0]
         if cls not in HBNBCommand.classes:
             print("** class doesn't exist **")
-            return
+            return False
+        return True
+
+    def check_id(arg):
+        """checks that id was passed and id exists
+        """
         if len(arg) < 2:
             print("** instance id missing **")
-            return
+            return False
         id = arg[1]
+        cls = arg[0]
         key = "{}.{}".format(cls, id)
         if key not in HBNBCommand.objects:
             print("** no instance found **")
-            return
-        print(HBNBCommand.objects[key])
+            return False
+        return True
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
